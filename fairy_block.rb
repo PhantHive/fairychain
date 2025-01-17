5cad824ec3c81c56b7beb8c6e444caed8fce21a8
PhantHive <phantomhive72@yahoo.fr>
2025-01-17 23:26:28 +0100

    feat: seperate class for creating and testing blockchain

---
+++ fairy_block.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+require 'digest'
+require 'pp'
+
+class FairyBlock
+  attr_reader :data
+  attr_reader :prev_hash
+  attr_reader :nonce
+  attr_reader :difficulty
+  attr_reader :time
+
+  def initialize(data, prev_hash, difficulty: '0000')
+    @data = data
+    @prev_hash = prev_hash
+    @difficulty = difficulty
+    @hash, @nonce, @time = compute_proof_of_work(difficulty)
+  end
+
+  def hash
+    Digest::SHA256.hexdigest("#{nonce}#{time}#{difficulty}#{prev_hash}#{data}")
+  end
+
+  def compute_proof_of_work(difficulty)
+    nonce = 0
+    loop do
+      time = Time.now.to_i
+      hash = Digest::SHA256.hexdigest( "#{nonce}#{time}#{difficulty}#{prev_hash}#{data}" )
+      if hash.start_with?(difficulty)
+        pp "Success: #{nonce} - Hash: #{hash} - Difficulty: #{difficulty} - Data: #{data}"
+        return [hash, nonce, time]
+      else
+        nonce += 1
+      end
+    end
+  end
+end
+
+
---
+++ fairy_chain.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+require 'pp'
+require_relative 'fairy_block'
+
+
+class FairyChain
+  attr_reader :blockchain
+
+  def initialize
+    @blockchain = []
+  end
+
+  def add_block(block)
+    @blockchain << block
+  end
+
+  def validate_fairychain?
+    @blockchain.each_with_index do |block, index|
+      if index == 0
+        init_hash = '0000000000000000000000000000000000000000000000000000000000000000'
+        unless block.instance_variable_get(:@prev_hash) == init_hash
+          pp "Initial block hash is not equal to the initial hash"
+          return false
+        end
+      else
+        prev_block = @blockchain[index - 1]
+        unless block.instance_variable_get(:@prev_hash) == prev_block.instance_variable_get(:@hash)
+          pp "Previous hash is not equal to the previous block's hash"
+          return false
+        end
+      end
+      unless block.instance_variable_get(:@hash).start_with?(block.difficulty)
+        return false
+      end
+    end
+  end
+end
+
+# mining blocks
+
+init_hash = '0000000000000000000000000000000000000000000000000000000000000000'
+b0 = FairyBlock.new('Hello, Fairies!', init_hash)
+b1 = FairyBlock.new('Hello, Fairies! I repeat HELLO FAIRIES!', b0.hash)
+b2 = FairyBlock.new('That\'s me Mario!', b1.hash)
+b3 = FairyBlock.new('TEST TEST TEST', b2.hash)
+
+
+blockchain = FairyChain.new
+blockchain.add_block(b0)
+blockchain.add_block(b1)
+blockchain.add_block(b2)
+blockchain.add_block(b3)
+
+
+if blockchain.validate_fairychain?
+  puts "Blockchain is operational"
+else
+  puts "Blockchain corrupted!"
+end
+
